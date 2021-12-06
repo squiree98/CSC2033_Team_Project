8f8d95da18d243edf857a33bca1d81380423c9f3
Kiara <c003770@newcastle.ac.uk>
2021-12-06 15:27:17 +0000

    Rename files and add an index view function

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# CONFIG
 app = Flask(__name__)
 
 
+# HOME PAGE VIEW
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
