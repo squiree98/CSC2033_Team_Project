4b6f34314187a62f08363faa014d6837c36b2fe5
Kiara <c003770@newcastle.ac.uk>
2021-12-07 19:53:08 +0000

    Add configuration to connect app to database and create classes which will map to tables in the database

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # CONFIG
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csc2033_team27_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://csc2033_team27:Lobe?CamJest@cs-db.ncl.ac.uk/csc2033_team27'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 # HOME PAGE VIEW
 @app.route('/')
--- models.py
+++ models.py
@@ -1,12 +1,96 @@
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://csc2033_team27:Lobe?CamJest!@cs-db.ncl.ac.uk:3306/csc2033_team27")
-Base = declarative_base()
+from flask_login import UserMixin
+from app import db
+from datetime import datetime
 
 
 def init_db():
-    Base.metadata.create_all(engine)
+
+    db.drop_all()
+    db.create_all()
 
 
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
 
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+    subscribed = db.Column(db.Boolean, nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    last_logged_in = db.Column(db.DateTime, nullable=True)
+    currently_logged_in = db.Column(db.DateTime, nullable=True)
+    average_score = db.Column(db.Integer, nullable=True)
+
+    created_quiz = db.relationship('Quiz')
+    quiz_scores = db.relationship('Score')
+
+    def __init__(self, first_name, last_name, email, password, role, subscribed):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.role = role
+        self.subscribed = subscribed
+        self.registered_on = datetime.now()
+        self.last_logged_in = None
+        self.currently_logged_in = None
+        self.average_score = 0
+
+
+class Quiz(db.Model):
+    __tablename__ = 'quiz'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)  # foreign key
+    name = db.Column(db.Text, nullable=False, default=False)
+    category = db.Column(db.String, primary_key=True)
+    number_of_plays = db.Column(db.Integer, primary_key=True)
+
+    scores = db.relationship('Score')
+    questions_and_answers = db.relationship('QuestionAndAnswers')
+
+    def __init__(self, user_id, name, category):
+        self.user_id = user_id
+        self.name = name
+        self.category = category
+        self.number_of_plays = 0
+
+
+class Score(db.Model):
+    __tablename__ = 'score'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # foreign keys
+    quiz_id = db.Column(db.String, db.ForeignKey(Quiz.id), nullable=False)
+    user_id = db.Column(db.String, db.ForeignKey(User.id), nullable=False)
+
+    score_value = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, quiz_id, user_id, score_value):
+        self.quiz_id = quiz_id
+        self.user_id = user_id
+        self.score_value = score_value
+
+
+class QuestionAndAnswers(db.Model):
+    __tablename__ = 'question_and_answers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.String, db.ForeignKey(Quiz.id), nullable=False)  # foreign key
+    question = db.Column(db.Text, nullable=False, default=False)
+    option_1 = db.Column(db.Text, nullable=False, default=False)
+    option_2 = db.Column(db.Text, nullable=False, default=False)
+    option_3 = db.Column(db.Text, nullable=False, default=False)
+    answer = db.Column(db.Text, nullable=False, default=False)
+
+    def __init__(self, quiz_id, question, option_1, option_2, option_3, answer):
+        self.quiz_id = quiz_id
+        self.question = question
+        self.option_1 = option_1
+        self.option_2 = option_2
+        self.option_3 = option_3
+        self.answer = answer
