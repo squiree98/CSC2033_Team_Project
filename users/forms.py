ff56fd0073d0807dc95d5468109d200b6cd08249
bpavlenk0 <ridertie@gmail.com>
2021-12-16 12:30:33 +0000

    Added validation to the register form

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class RegisterForm(FlaskForm):
     # each data field must be error checked
-    email = StringField()
+    email = StringField(validators=[DataRequired(), Email()])
 
-    password = PasswordField()
-    confirm_password = PasswordField()
+    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15,message='Password must be between 8 and 15 characters in length.')])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField()
 
 
