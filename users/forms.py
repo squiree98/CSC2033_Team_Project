bb4fa3b09dbc721182b907c3d9217777c6999cfa
bpavlenk0 <ridertie@gmail.com>
2021-12-17 14:08:46 +0000

    Added placeholders for the registration questionnaire

--- users/forms.py
+++ users/forms.py
@@ -9,6 +9,11 @@ class RegisterForm(FlaskForm):
 
     password = PasswordField(validators=[DataRequired(), Length(min=8, max=15,message='Password must be between 8 and 15 characters in length.')])
     confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    q1 = StringField(validators=[DataRequired()])
+    q2 = StringField(validators=[DataRequired()])
+    q3 = StringField(validators=[DataRequired()])
+    q4 = StringField(validators=[DataRequired()])
+    q5 = StringField(validators=[DataRequired()])
     submit = SubmitField()
 
 
