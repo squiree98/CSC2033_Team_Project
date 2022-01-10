659f4f4bade52a0641f2767602a7e9d99d141960
squiree98 <c0051039@newcastle.ac.uk>
2022-01-10 15:39:23 +0000

    Implemented share functionality

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_share import Share
+
+# create share object
+share = Share()
 
 # CONFIG
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://csc2033_team27:Lobe?CamJest@cs-db.ncl.ac.uk:3306/csc2033_team27'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'csc2033_team27_key'
+
+
+share.init_app(app)
 
 
 db = SQLAlchemy(app)
